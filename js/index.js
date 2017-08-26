af839fee10bb1ce4fb1263ae5612abe29d079a4c
xhq <test@runoob.com>
2017-08-26 16:05:58 +0800

    首页主体

--- js/index.js
+++ js/index.js
@@ -16,17 +16,26 @@ require(["config"],function(){
 			$(".brand-img").attr({
 				src:data.brand1.src
 			})
-			console.log(data.brand1.src1);
+//			console.log(data.brand1.src1);
 			$(".brand-img0").attr({
 				src:data.brand2.src
 			})
+//			$(".banner_img").attr({
+//				src:data.brand3.src
+//			})
 		})
 		$.getJSON("../mock/discount.json",function(data){
 			var html = arttemplate("mainitem",{list:data})
 		$(html).appendTo(".discount")
 		})
+//		$.getJSON("../mock/main.json",function(data){
+////			console.log(data);
+//			var html = arttemplate("maizn",{list:data})
+////			$(html).appendTo(".all")
+//			console.log(html);
+//		})
 		var _html = arttemplate("discount",{list:6});
-		console.log(_html);
+//		console.log(_html);
 		$(_html).appendTo(".cate-list")
 		
 	})
