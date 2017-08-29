1a78a8a041aec5be4affd981c76ac7a0a484b5e4
xhq <test@runoob.com>
2017-08-29 20:45:03 +0800

    啦啦啦

--- js/goodList0.js
+++ js/goodList0.js
@@ -3,16 +3,24 @@ require(["config"],function(){
 //	alert(11)
 	}),
 require(["jquery","arttemplate"],function($,arttemplate){
-//		$.getJSON("../mock/goodList.json",function(data){
-////			alert(11)
-//		var html = arttemplate("goodList",{list:data})
-//			console.log(html);
-//		})
-	$.getJSON("../mock/goodList.json",function(data){
-		alert(11)
+//	alert(11)
+	$.ajax({
+		type:"get",
+		url:"../mock/goodList.json",
+		async:true,
+		success:function(data){
+//			alert(11)
 		var html = arttemplate("goodList",{list:data})
-		console.log(html);
-//		$(html).appendTo(".main-sell-list")
-		})
+		$(html).appendTo(".bigall1");
+		$(".bigall1 li").mouseenter(function(){
+//	$(this).find(".option").css("display","block")
+	$(this).find(".bgoption").css("display","block")
+})
+$(".bigall1 li").mouseleave(function(){
+//	$(this).find(".search-img .option").css("display","none")
+	$(this).find(".bgoption").css("display","none")
+})
+		}
+	});
 	})
 })
